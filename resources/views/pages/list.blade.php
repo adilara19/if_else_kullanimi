2861ded4c43cd990bc7cf2369a64a2f7a2e2c643
Ayşe Dilara Öztürk <adilaraozturk@gmail.com>
2023-08-16 16:13:43 +0300

    list blade eklendi

---
+++ resources/views/pages/list.blade.php
@@ -0,0 +1,25 @@
+burası list blade
+
+@php
+
+$data = '3';
+
+@endphp
+
+@switch($data)
+
+@case(1)
+numara 1'dir
+@break
+
+@case(2)
+numara 2'dir
+@break
+
+@case(3)
+numara 3'tür
+@break
+
+@defalut
+default deger
+@endswitch
