9a7692e87978015123c55e309f0ad105610ae936
Ayşe Dilara Öztürk <adilaraozturk@gmail.com>
2023-08-16 16:13:24 +0300

    student blade eklendi

---
+++ resources/views/pages/students.blade.php
@@ -0,0 +1,19 @@
+<center>
+
+    Burası Students Sayfası <hr>
+    
+    @if(count($students))
+        @foreach($students as $student)
+        <li>{{$student}}</li><br>
+        @endforeach
+        <br>
+        <br>
+    
+        @foreach($class as $c)
+        <li>{{$c}}</li>
+        @endforeach
+    @endif    
+    
+    </center>
+    
+    
